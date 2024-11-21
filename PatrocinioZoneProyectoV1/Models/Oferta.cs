22042128e1c3fd4de7f9015da0d1e6a94942a3e7
Maria Isabel Bazan <misabelbsalih@gmail.com>
2024-11-20 22:45:13 -0300

    Clase Oferta

--- PatrocinioZoneProyectoV1/Models/Oferta.cs
+++ PatrocinioZoneProyectoV1/Models/Oferta.cs
@@ -9,17 +9,7 @@ namespace PatrocinioZoneProyectoV1.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int OfertaId { get; set; }
 
-        [Display(Name = "Nombre")]
-        [Required(ErrorMessage = "Por favor cargar el nombre!")]
-        public string Nombre { get; set; }
-
-        [Display(Name = "Email")]
-        [Required(ErrorMessage = "Por favor cargar el email!")]
-        public string Email { get; set; }
-
-        [Display(Name = "Fecha de ingreso")]
-        public DateTime FechaIngreso { get; set; }
-
+       
 
     }
 }
